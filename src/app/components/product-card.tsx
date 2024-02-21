487098d9b437d68a64d2394f13f2b630f8e6923e
Bruno Santana <brunoliveiraq@gmail.com>
2024-02-21 12:26:43 -0300

    [changed] styles and consts

--- src/app/components/product-card.tsx
+++ src/app/components/product-card.tsx
@@ -1,67 +1,72 @@
-import styled from "styled-components";
+import { useRouter } from "next/navigation"
+import { styled } from "styled-components"
 import { formatPrice } from "../utils/format-price";
 
-interface ProductCardProps {
-    img: string;
-    title: string;
-    price: number;
 
+interface ProductCardProps {
+    img: string,
+    title: string,
+    price: number,
+    id: string
 }
 
 const Card = styled.div`
-            display: flex;
-            align-items: start;
-            justify-content: center;
-            flex-direction: column;
-            background: rgba (255, 255, 255, 0.4);
-            backdrop-filter: blur(10px);
-            border-radius: 0px, 0px, 4px, 4px;
-            width: 256px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    cursor: pointer;
 
-            img {
-                width: 256px;
-                height: 258px;    
-                border-radius: 10px;
-                margin-top: 30px;
-                
-            }
+    background: rgba(255, 255, 255, 0.4);
+    backdrop-filter: blur(10px);
+    border-radius: 0px 0px 4px 4px;
 
+    width: 256px;
 
-            h3 {
-                font-weight: 300;
-                font-size: 16px;
-                line-height: 150%
-                color: var(--text-dark2);
+    img {
+        width: 256px;
+        height: 300px;
+    }
 
-            }
+    h3 {
+        font-weight: 300;
+        font-size: 16px;
+        line-height: 150%;
+        color: var(--text-dark-2);
+    }
 
-            p {
-                font-size: 14px;
-                font-weight: 600;
-                line-height: 150%;
-                color: var(--shapes-dark);
-                text-aligen: left;
+    p {
+        font-weight: 600;
+        font-size: 14px;
+        line-height: 150%;
+        color: var(--shapes-dark);
+    }
 
-            }
-
-
-            > div {
-                width: 228px;
-                height: 1px;
-                margin: 8px 0;
-                background: var(--shapes);
-            }
-
+    div {
+        display: flex;
+        align-items: start;
+        justify-content: center;
+        flex-direction: column;
+        padding: 8px 12px;
+        width: 100%;
+    }
 `
 
-export function ProductCard(props: ProductCardProps) {
+export function ProductCard(props : ProductCardProps){
+    const router = useRouter();
     const price = formatPrice(props.price)
 
-    return (
-        <Card>
+    const handleNavigate = () => {
+        router.push("/product?id=" + props.id);
+    }
+
+    return(
+        <Card onClick={handleNavigate}>
             <img src={props.img}/>
-            <h3>{props.title}</h3>
-            <p>{price}</p>
+            <div>
+                <h3>{props.title}</h3>
+                <p>{price}</p>
+            </div>
         </Card>
     )
 }
